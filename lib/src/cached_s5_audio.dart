1056f730c93d5e2a303c858df81e1383080b5c29
covalent <mail@lukehmcc.com>
2024-08-06 15:47:38 -0400

    feat: complete audio scrubbing

--- lib/src/cached_s5_audio.dart
+++ lib/src/cached_s5_audio.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 
 import 'package:audioplayers/audioplayers.dart';
 import 'package:cached_s5_manager/cached_s5_manager.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:s5/s5.dart';
@@ -21,27 +22,40 @@ class CachedS5AudioState extends State<CachedS5Audio> {
   Uint8List? audioBytes;
   final player = AudioPlayer();
   Logger logger = Logger();
-  int position = 0;
+  Duration? _duration;
+  Duration? _position;
   late CachedS5Manager cacheManager;
+  String get _durationText => _duration?.toString().split('.').first ?? '';
+  String get _positionText => _position?.toString().split('.').first ?? '';
+
+  @override
+  void initState() {
+    cacheManager = CachedS5Manager(s5: widget.s5);
+    _populateAudioFromCID();
+    super.initState();
+  }
 
   // This grabs the contents of the CID
   _populateAudioFromCID() async {
-    // FI
     Uint8List audioBytesLoc = await cacheManager.getBytesFromCID(widget.cid);
     await player.setSourceBytes(audioBytesLoc);
     setState(() {
       audioBytes = audioBytesLoc;
     });
-    player.onPositionChanged.listen((event) {
+    player.onDurationChanged.listen((duration) => setState(() {
+          _duration = duration;
+        }));
+    player.onPositionChanged.listen((event) async {
       // keep the setstates to 1 per second to keep redraws to a min
-      if (event.inSeconds != position) {
+      if (event != _position) {
         setState(() {
-          position = event.inSeconds;
+          _position = event;
         });
       }
     });
   }
 
+  // Toggles play and pause based on current state
   _playPause() async {
     if (player.state == PlayerState.playing) {
       await player.pause();
@@ -50,13 +64,6 @@ class CachedS5AudioState extends State<CachedS5Audio> {
       await player.resume();
       setState(() {});
     }
-  }
-
-  @override
-  void initState() {
-    cacheManager = CachedS5Manager(s5: widget.s5);
-    _populateAudioFromCID();
-    super.initState();
   }
 
   @override
@@ -73,6 +80,39 @@ class CachedS5AudioState extends State<CachedS5Audio> {
                       : Icons.play_arrow))
               : CircularProgressIndicator(),
         ),
+        Stack(
+          children: [
+            Slider(
+              onChanged: (value) {
+                final duration = _duration;
+                if (duration == null) {
+                  return;
+                }
+                final position = value * duration.inMilliseconds;
+                player.seek(Duration(milliseconds: position.round()));
+              },
+              value: (_position != null &&
+                      _duration != null &&
+                      _position!.inMilliseconds > 0 &&
+                      _position!.inMilliseconds < _duration!.inMilliseconds)
+                  ? _position!.inMilliseconds / _duration!.inMilliseconds
+                  : 0.0,
+            ),
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  _position != null
+                      ? '$_positionText / $_durationText'
+                      : _duration != null
+                          ? _durationText
+                          : '',
+                  style: const TextStyle(fontSize: 8.0),
+                ),
+              ),
+            )
+          ],
+        )
       ],
     );
   }
